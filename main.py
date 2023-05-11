ee577759b214756bdce57e369bf139b2dec6d193
Jafar1337 <103189084+Jafar1337@users.noreply.github.com>
2023-05-11 12:18:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
 
 from flask import Flask
-from flask import Flask, render_template
-from flask import request
-from flask import abort, redirect, url_for, make_response
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
- return render_template('index.html')
-
-@app.route("/help")
-def help():
- return "help me"
-
-@app.route('/contact')
-def contact():
- return app.send_static_file('contact.html')
-
-@app.route('/about')
-def about():
- return app.send_static_file('about.html')
-
-@app.route('/gallery')
-def gallery():
- return app.send_static_file('gallery.html')
-
- @app.route('/contact')
- def contact():
-  return app.send_static_file('contact.html')
-
+    return "Witaj na stronie głównej!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
